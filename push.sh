abb5c2fd319e23b757977d8edf06eb9827e2af61
ZzzhangLK <656650937@qq.com>
2021-07-10 17:03:57 +0800

    push

--- push.sh
+++ push.sh
@@ -1,4 +1,7 @@
 git init
+git branch -a
+git checkout blogcode
 git add -A
-git commit -m 'push'
+git commit -m "push"
 git push -f git@github.com:ZzzhangLK/ZzzhangLK.github.io.git blogcode
+git checkout master
